ab84ecff7b1c1e77782fcfb543abe9b0e483eee6
Luka Gogichaishvili <luka595664@gmail.com>
2025-05-02 15:21:08 +0400

    [SETUP] Initializing project structure
    Using Nest CLI to quickly create resource folders.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ArticlesModule } from './articles/articles.module';
+import { CategoriesModule } from './categories/categories.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ArticlesModule, CategoriesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
