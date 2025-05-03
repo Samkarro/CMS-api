44d6260b0849d8f1668d6b410a38508b41116d5c
Luka Gogichaishvili <luka595664@gmail.com>
2025-05-03 12:05:22 +0400

    [SETUP] Added database connection
    Postgres database added. Possible issues may arise which will require changes, or a complete redo of these steps.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,26 @@ import { AuthModule } from './auth/auth.module';
 import { ArticlesModule } from './articles/articles.module';
 import { CategoriesModule } from './categories/categories.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, ArticlesModule, CategoriesModule, UsersModule],
+  imports: [
+    AuthModule,
+    ArticlesModule,
+    CategoriesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }), // testt ss
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
