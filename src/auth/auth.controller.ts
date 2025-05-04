e97f6a8399d505a7214985a240900492cadb86a6
Luka Gogichaishvili <luka595664@gmail.com>
2025-05-04 16:51:13 +0400

    [FIX] Refactored to match endpoint requirements
    Made it so that the auth route doesn't have endpoints and that the users route has the same functionality auth would have. Imported auth.service.ts into users.controller.ts so the route would match /users/* as stated in the requirements. Kept auth.guard.ts to implement in article creation route.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {
-  constructor(private readonly authservice: AuthService) {}
-
-  @Post('register')
-  async register(
-    @Body('username') username: string,
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ) {
-    return this.authservice.register(username, email, password);
-  }
-
-  @Post('login')
-  async login(
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ) {
-    return this.authservice.login(email, password);
-  }
-
-  // possible endpoint? TODO: look up how to use guards here
-}
+export class AuthController {}
