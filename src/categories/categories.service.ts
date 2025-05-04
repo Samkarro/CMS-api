b646c5c058dc506f7f6b6e3c1759cf24daed52b8
Luka Gogichaishvili <luka595664@gmail.com>
2025-05-04 18:13:59 +0400

    [FEATURE] Created category service
    Serves list, create and delete endpoints

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './categories.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private categoriesRepostiory: Repository<Category>,
+  ) {}
+
+  list() {
+    return this.categoriesRepostiory.find();
+  }
+
+  create(categoryName: string): Promise<Category> {
+    const newCategory = this.categoriesRepostiory.create({ categoryName });
+    return this.categoriesRepostiory.save(newCategory);
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.categoriesRepostiory.delete(id);
+  }
+}
