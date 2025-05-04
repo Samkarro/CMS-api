12e1cedef4849f77c82914c3d5515799c796d98f
Luka Gogichaishvili <luka595664@gmail.com>
2025-05-04 19:06:21 +0400

    [FEATURE] Created articles entity
    Entity has One to Many relationship to User entity and a Many to Many relationship with Category. The defunct articles interface has been removed.

---
+++ src/articles/atricles.entity.ts
@@ -0,0 +1,26 @@
+import { Category } from 'src/categories/categories.entity';
+import { User } from 'src/users/users.entity';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('articles')
+export class Article {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @ManyToOne(() => User, (user) => user.articles)
+  author: User;
+
+  @ManyToMany(() => Category)
+  @JoinTable()
+  categories: Category[];
+}
