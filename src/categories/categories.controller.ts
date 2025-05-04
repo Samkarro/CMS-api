3b458df27747a16c7a14217b61024d7c670b7767
Luka Gogichaishvili <luka595664@gmail.com>
2025-05-04 18:19:13 +0400

    [FEATURE] Created category controller
    Serves list, create and delete endpoints

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private categoriesService: CategoriesService) {}
+
+  @Get()
+  async list() {
+    return this.categoriesService.list();
+  }
+
+  @Post()
+  async create(@Body('categoryName') categoryName: string) {
+    return this.categoriesService.create(categoryName);
+  }
+
+  @Delete()
+  async delete(@Param('id') id: number) {
+    return this.categoriesService.delete(id);
+  }
+}
