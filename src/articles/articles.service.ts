6d16554670b905563c681b17412e36777b619280
Luka Gogichaishvili <luka595664@gmail.com>
2025-05-07 20:09:55 +0400

    [FIX] Article update works as intended now.

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,11 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Article } from './entities/articles.entity';
 import { User } from 'src/users/entities/users.entity';
 import { Category } from 'src/categories/entities/categories.entity';
 import { AuthService } from 'src/auth/auth.service';
-import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class ArticlesService {
@@ -15,11 +19,25 @@ export class ArticlesService {
 
     @InjectRepository(Category)
     private categoriesRepository: Repository<Category>,
+
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
     private authService: AuthService,
   ) {}
 
-  list() {
-    const articleList = this.articlesRepository.find();
+  async list(): Promise<Article[]> {
+    const articleList = await this.articlesRepository
+      .createQueryBuilder('article')
+      .leftJoinAndSelect('article.categories', 'category')
+      .leftJoinAndSelect('article.author', 'user')
+      .select([
+        'article.id',
+        'article.title',
+        'article.body',
+        'user.username',
+        'category.categoryName',
+      ])
+      .getMany();
     if (!articleList) {
       throw new NotFoundException('No articles found in database');
     }
@@ -67,7 +85,19 @@ export class ArticlesService {
   }
 
   async findById(id): Promise<Article> {
-    const article = await this.articlesRepository.findOneBy({ id });
+    const article = await this.articlesRepository
+      .createQueryBuilder('article')
+      .leftJoinAndSelect('article.categories', 'category')
+      .leftJoinAndSelect('article.author', 'user')
+      .select([
+        'article.id',
+        'article.title',
+        'article.body',
+        'user.username',
+        'category.categoryName',
+      ])
+      .where('article.id = :id', { id })
+      .getOne();
     if (!article) {
       throw new NotFoundException('Article not found');
     }
@@ -84,11 +114,43 @@ export class ArticlesService {
 
   async update(body, id): Promise<Article> {
     // TODO: also do it here
-    const article = await this.articlesRepository.findOneBy({ id });
+    const article = await this.articlesRepository
+      .createQueryBuilder('article')
+      .leftJoinAndSelect('article.categories', 'category')
+      .leftJoinAndSelect('article.author', 'user')
+      .select([
+        'article.id',
+        'article.title',
+        'article.body',
+        'user.username',
+        'user.password',
+        'category.categoryName',
+      ])
+      .where('article.id = :id', { id })
+      .getOne();
+
     if (!article) {
       throw new NotFoundException('Article not found');
     }
-    this.articlesRepository.update(id, body);
-    return this.articlesRepository.findOneBy({ id });
+    if (!body.user) {
+      throw new ForbiddenException('User not specified');
+    }
+    const user = await this.authService.findOneByEmail(body.user.email);
+    if (!user) {
+      throw new NotFoundException('Passed user not found');
+    }
+    if (body.user.email !== user.email) {
+      throw new BadRequestException('Cannot edit user');
+    }
+    this.authService.validateUser(body.user.email, body.user.password);
+
+    const updatedArticle = this.articlesRepository.create({
+      ...article,
+      ...body,
+      author: user, // Ensure the author is assigned correctly
+    });
+
+    await this.articlesRepository.save(updatedArticle);
+    return await this.articlesRepository.findOneBy({ id });
   }
 }
