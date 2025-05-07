0012d71fc68bb5f19ea32f17cfa0297af7820462
Luka Gogichaishvili <luka595664@gmail.com>
2025-05-08 02:11:40 +0400

    [FEATURE] Documented articles module

---
+++ src/common/dtos/resources/articles/UpdateArticleDto.dto.ts
@@ -0,0 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+export class updateArticleDto {
+  @ApiProperty({
+    example: 'Is Pineapple on Pizza Slowly Entering the Mainsteam?',
+    required: false,
+  })
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({
+    example: {
+      email: 'romanceScience@gmail.com',
+      password: 'fnwebofg73297',
+    },
+    required: true,
+  })
+  user: {
+    email: string;
+    password: string;
+  };
+
+  @ApiProperty({
+    example: ['Politics', 'Lifestyle'],
+    required: false,
+  })
+  categories: string[];
+
+  @ApiProperty({
+    example:
+      'Some people believe that pineapple on pizza is an abomination, but a growing population of new enthusiasts of Italian quisine would like to disagree',
+    required: false,
+  })
+  body: string;
+}
