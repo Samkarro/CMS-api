51431dde04f910c3d865d0f9d16791baa5855074
Luka Gogichaishvili <luka595664@gmail.com>
2025-05-08 00:22:33 +0400

    [FEATURE] Added swagger module to app, created base path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('CMS REST API')
+    .setDescription('Simple cms api')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addTag('Articles')
+    .addTag('Users')
+    .addTag('Categories')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, documentFactory);
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
