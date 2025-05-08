7b14b16f89e6c6f8a97356fb03100a46930eba72
Luka Gogichaishvili <luka595664@gmail.com>
2025-05-08 21:10:57 +0400

    [FEATURE] Created mocks for articles service tests
    Decided to push things slowly

--- src/articles/articles.service.spec.ts
+++ src/articles/articles.service.spec.ts
@@ -4,6 +4,32 @@ import { ArticlesService } from './articles.service';
 describe('ArticlesService', () => {
   let service: ArticlesService;
 
+  const mockArticlesRepository = {
+    createQueryBuilder: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    findOneBy: jest.fn(),
+  };
+
+  const mockCategoriesRepository = {
+    find: jest.fn(),
+    save: jest.fn(),
+    delete: jest.fn(),
+    create: jest.fn(),
+    findOneBy: jest.fn(),
+  };
+
+  const mockI18nService = {
+    t: jest.fn(),
+  };
+
+  const mockAuthService = {
+    login: jest.fn(),
+    register: jest.fn(),
+    validateUser: jest.fn(),
+    findOneByEmail: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ArticlesService],
